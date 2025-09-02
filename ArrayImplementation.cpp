f9cb7dcb3927ab1aab6164ce48efecdaadfa4079
Vaidisha <vaidishamisra@gmail.com>
2025-09-02 20:57:17 +0530

    Bubble Sort

--- ArrayImplementation.cpp
+++ ArrayImplementation.cpp
@@ -166,6 +166,20 @@ class Array{
         cout<<"element not found in array"<<endl;
         return -1;
     }
+    void bubbleSort(){
+        int n=size;
+        int flag;
+        for(int i=0;i<n-1;i++){
+            flag=0;
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    flag=1;
+                    swap(arr[j],arr[j+1]);
+                }
+            }
+            if(flag==0) break;
+        }
+     }
 };
 int main(){
     Array a1;
@@ -230,6 +244,10 @@ int main(){
                 cin>>el;
                 a1.binarySearch(el);
                 break;
+            case 12:
+               a1.bubbleSort();
+               a1.Print();
+               break;
         
         }
         op--;
