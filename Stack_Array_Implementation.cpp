e225a349244a43470b3bbc2da284cfcbf6363891
Vaidisha <vaidishamisra@gmail.com>
2025-09-20 17:34:18 +0530

    Array Implementation of Stack

--- Stack_Array_Implementation.cpp
+++ Stack_Array_Implementation.cpp
@@ -1,62 +1,73 @@
+
 #include <iostream>
 using namespace std;
 
-class Stack{
-    int top,capacity;
+class myStack {
     int *arr;
+    int capacity;
+    int top;        
 
     public:
-        Stack(int size){
-            this->capacity=size;
-            top=-1;
-            arr= new int[capacity];
-        }
 
-        ~Stack(){
-            delete[] arr;
-        }
+    myStack(int cap) {
+        capacity = cap;
+        arr = new int[capacity];
+        top = -1;
+    }
 
-        void push(int val){
-            if(top>=capacity-1){
-                cout<<"Stack Overflow"<<endl;
-                return;
-            }
-            arr[++top]=val;
+    void push(int x) {
+        if (top >= capacity - 1) {
+            cout << "Stack Overflow\n";
+            return;
         }
+        arr[++top] = x;
+    }
 
-        int pop(){
-            if(top<0){
-                cout<<"Stack Underflow"<<endl;
-                return -1;
-            }
-            return arr[top--];
+    int pop() {
+        if (top<0) {
+            cout << "Stack Underflow\n";
+            return -1;
         }
+        return arr[top--];
+    }
 
-        int peek(){
-            if(top<0){
-                cout<<"Stack Underflow"<<endl;
-                return -1;
-            }
-            return arr[top];
+    
+    int peek() {
+        if (top<0) {
+            cout << "Stack is Empty\n";
+            return -1;
         }
-        
-        bool isEmpty(){
-            return top<0;
-        }
+        return arr[top];
+    }
 
-        int size(){
-            return top+1;
-        }
+    bool isEmpty() {
+        return top<0;
+    }
 
+    bool isFull() {
+        return top == capacity - 1;
+    }
+
+    int size(){
+        return top+1;
+    }
 };
 
-int main(){
-    Stack S(5);
-    S.push(1);
-    S.push(2);
-    S.push(3);
-    S.push(4);
-    cout<<S.peek()<<endl;
+int main() {
+    myStack st(4);
 
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
 
+    cout << "Popped: " << st.pop() << "\n";
+
+    cout << "Top element: " << st.peek() << "\n";
+
+    cout << "Is stack empty: " << (st.isEmpty() ? "Yes" : "No") << "\n";
+
+    cout << "Is stack full: " << (st.isFull() ? "Yes" : "No") << "\n";
+
+    return 0;
 }
